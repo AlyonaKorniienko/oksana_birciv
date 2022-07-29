0793d10166f476bc3e3956ca48e861cb2ad160b8
AlyonaKorniienko <vkusniashka.pp.ua@gmail.com>
2022-07-29 09:20:07 +0300

    hero apdated

--- js/mobile.js
+++ js/mobile.js
@@ -1,12 +1,14 @@
 (() => {
   const refs = {
     openMobileBtn: document.querySelector('.mobile-open__btn'),
-    closeMobileBtn: document.querySelector('.mobile-close__btn'),
+    closeMobileItem: document.querySelector('.mobile-close__btn'),
+    closeMobileBtn: document.querySelector('.nav__item'),
     mobile: document.querySelector('.mobile-menu'),
   };
 
   refs.openMobileBtn.addEventListener('click', toggleMobile);
   refs.closeMobileBtn.addEventListener('click', toggleMobile);
+  refs.closeMobileItem.addEventListener('click', toggleMobile);
 
   function toggleMobile() {
     document.body.classList.toggle('mobile-open');
